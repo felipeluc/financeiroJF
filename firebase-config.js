7511c3fa90363b98fe585aba94a506c1299d8d70
felipeluc <felipelucaspereira246@gmail.com>
2025-11-17 11:40:46 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// Firebase configuration and initialization
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAFyfqpl4rNBE4gIXllX7W66f43RSMaDCg",
   authDomain: "gestaoprojetos-18968.firebaseapp.com",
@@ -13,14 +12,6 @@ const firebaseConfig = {
   appId: "1:862351920002:web:97405d73b4804c2cb85d9b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-export default app;
-
